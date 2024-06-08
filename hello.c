298f67181955ca31609e2cdcd92d6ec54bc8e662
BlueDragon320 <vaibhav.b.g.patil@gmail.com>
2024-06-08 21:17:55 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,44 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     printf("Hello, World\n");
+// }
+
+
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char answer[100];
+//     printf("What's your name? ");
+//     scanf("%s", answer);
+//     printf("hello, %s\n", answer);
+// }
+
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("What is the value of x? ");
+//     int x;
+//     scanf("%d", &x);
+
+//     printf("What is the value of y? ");
+//     int y;
+//     scanf("%d", &y);
+    
+//     if (x>y)
+//     {
+//         printf("x is greater than y\n");
+//     }
+//     else if (x<y)
+//     {
+//         printf("x is less than y\n");
+//     }
+//     else
+//     { 
+//         printf("x is equal to y\n");
+//     }
+// }
