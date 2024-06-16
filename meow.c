a3634a71d8027e32e0802202ed482aa394767a0a
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-06-16 23:05:11 +0530

    Add files via upload

---
+++ meow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i=3;
+    while(i>0)
+    {
+        printf("meow\n");
+        i--;
+    }
+}
+
+
+// it could have been done like this
+// {
+// for (int i=0; i<3; i++)
+// {
+//     printf("meow\n");
+// }
+// }
