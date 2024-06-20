83d56548cdc95e08233e63b0dcd2255f18687750
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-06-20 23:28:13 +0530

    Add files via upload

---
+++ l2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+}
+
+
+// pre processing [Normal code]
+// compiler [vscode which converts code to assembly code]
+// assmebly [assembly code to binary 0's and 1's]
+// linking [combines all binary data]
